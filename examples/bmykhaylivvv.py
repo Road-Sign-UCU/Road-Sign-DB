8f55cf2cd2dde4d2e38a9262b4a7927f0e1e923c
bohdanmykhayliv <bohdan.mykhailiv@ucu.edu.ua>
2021-05-16 01:23:19 +0300

    Finished module for GermanRoadSignsDB

--- examples/bmykhaylivvv.py
+++ examples/bmykhaylivvv.py
@@ -56,6 +56,9 @@ class GermanTrafficSigns(BaseDataset):
         '''
         Doc
         '''
+        sign_classid = {'0': {'sign_class': 'PROHIBITORY', 'sign_type': '20_SIGN'}, '1': {'sign_class': 'PROHIBITORY', 'sign_type': '30_SIGN'}, '2': {'sign_class': 'PROHIBITORY', 'sign_type': '50_SIGN'}, '3': {'sign_class': 'PROHIBITORY', 'sign_type': '60_SIGN'}, '4': {'sign_class': 'PROHIBITORY', 'sign_type': '70_SIGN'}, '5': {'sign_class': 'PROHIBITORY', 'sign_type': '80_SIGN'}, '6': {'sign_class': 'OTHER', 'sign_type': 'OTHER'}, '7': {'sign_class': 'PROHIBITORY', 'sign_type': '100_SIGN'}, '8': {'sign_class': 'PROHIBITORY', 'sign_type': '120_SIGN'}, '9': {'sign_class': 'PROHIBITORY', 'sign_type': 'OTHER'}, '10': {'sign_class': 'PROHIBITORY', 'sign_type': 'OTHER'}, '11': {'sign_class': 'WARNING', 'sign_type': 'OTHER'}, '12': {'sign_class': 'INFORMATION', 'sign_type': 'PRIORITY_ROAD'}, '13': {'sign_class': 'WARNING', 'sign_type': 'GIVE_WAY'}, '14': {'sign_class': 'WARNING', 'sign_type': 'STOP'}, '15': {'sign_class': 'PROHIBITORY', 'sign_type': 'OTHER'}, '16': {'sign_class': 'PROHIBITORY', 'sign_type': 'OTHER'}, '17': {'sign_class': 'PROHIBITORY', 'sign_type': 'OTHER'}, '18': {'sign_class': 'WARNING', 'sign_type': 'OTHER'}, '19': {'sign_class': 'WARNING', 'sign_type': 'OTHER'}, '20': {'sign_class': 'WARNING', 'sign_type': 'OTHER'}, '21': {
+            'sign_class': 'WARNING', 'sign_type': 'OTHER'}, '22': {'sign_class': 'WARNING', 'sign_type': 'OTHER'}, '23': {'sign_class': 'WARNING', 'sign_type': 'OTHER'}, '24': {'sign_class': 'WARNING', 'sign_type': 'OTHER'}, '25': {'sign_class': 'WARNING', 'sign_type': 'OTHER'}, '26': {'sign_class': 'WARNING', 'sign_type': 'OTHER'}, '27': {'sign_class': 'WARNING', 'sign_type': 'OTHER'}, '28': {'sign_class': 'WARNING', 'sign_type': 'OTHER'}, '29': {'sign_class': 'WARNING', 'sign_type': 'OTHER'}, '30': {'sign_class': 'WARNING', 'sign_type': 'OTHER'}, '31': {'sign_class': 'WARNING', 'sign_type': 'OTHER'}, '32': {'sign_class': 'PROHIBITORY', 'sign_type': 'OTHER'}, '33': {'sign_class': 'MANDATORY', 'sign_type': 'OTHER'}, '34': {'sign_class': 'MANDATORY', 'sign_type': 'OTHER'}, '35': {'sign_class': 'MANDATORY', 'sign_type': 'OTHER'}, '36': {'sign_class': 'MANDATORY', 'sign_type': 'OTHER'}, '37': {'sign_class': 'MANDATORY', 'sign_type': 'OTHER'}, '38': {'sign_class': 'MANDATORY', 'sign_type': 'PASS_RIGHT_SIDE'}, '39': {'sign_class': 'MANDATORY', 'sign_type': 'PASS_RIGHT_SIDE'}, '40': {'sign_class': 'MANDATORY', 'sign_type': 'OTHER'}, '41': {'sign_class': 'PROHIBITORY', 'sign_type': 'OTHER'}, '42': {'sign_class': 'PROHIBITORY', 'sign_type': 'OTHER'}}
+
         dataset_f = open(self.app_dataset_filename, 'a')
         if not DEBUG:
             self.__unzip_files()
@@ -65,22 +68,43 @@ class GermanTrafficSigns(BaseDataset):
         dataset_f = open(self.app_dataset_filename, 'a')
         with open(self._annotations_path, 'r') as f:
             for line in f:
-
+                
                 image_name = line.split(';')[0].split('.')[0]
-                if image_name == 'Filename':
+                if image_name == 'Filename':  # skip first line
                     continue
+
+                data_line = line.split(';')
+                initial_size_x = data_line[1]
+                initial_size_y = data_line[2]
+                classid = data_line[-1].strip()
+
+                x1, y1, x2, y2 = int(data_line[3]), int(
+                    data_line[4]), int(data_line[5]), int(data_line[6])
+                x1, x2 = sorted((x1, x2))
+                y1, y2 = sorted((y1, y2))
 
                 # convert images from ppm to jpg.format
                 image = Image.open(self._at_mydir(image_name+'.ppm'))
                 os.remove(self._at_mydir(image_name+'.ppm'))
                 image.save(self._at_mydir(image_name+'.jpg'))
 
-                # resize images
-                img = cv2.imread(self._at_mydir(image_name+'.jpg'))
-                img = cv2.resize(
-                    img, (128, 128), interpolation=cv2.INTER_AREA
+                image_path_new = self.generate_new_image_file_path(
+                    'jpg')  # generate new image name
+                if not os.path.exists(image_path_new):
+                    # resize images
+                    img = cv2.imread(self._at_mydir(image_name+'.jpg'))
+                    img_conv = img[x1:x2, y1:y2]
+                    img_conv = cv2.resize(
+                        img_conv, (128, 128), interpolation=cv2.INTER_AREA
+                    )
+                    cv2.imwrite(image_path_new, img_conv)
+
+                # write image info to DATASET.csv
+                self.spa.add_entry(
+                    image_name=os.path.basename(image_path_new), initial_size_x=initial_size_x, initial_size_y=initial_size_y,
+                    country="GERMANY", occlusions="NO_INFO", sign_class=sign_classid[classid]['sign_class'], sign_type=sign_classid[classid]['sign_type']
                 )
-                cv2.imwrite(self._at_mydir(image_name+'.jpg'), img)
+        self.append_data_to_file()
 
 
 if __name__ == "__main__":
